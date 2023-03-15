69089729474084488aad12bc44281a9be527e99f
Ivan <v.gritskevich25@gmail.com>
2023-03-15 20:08:55 +0300

    initial

---
+++ WebApplicationWithActions.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace WebApplicationWithActions.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var actual = 10;
+
+            Assert.Equal(10, actual);
+        }
+    }
+}
